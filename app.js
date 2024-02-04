0c545343d342285e0c46c2779e3619269e918e1f
arim83 <jorammercado@pursuit.org>
2024-02-04 06:19:58 -0500

    seperate server and app

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT;
+const BASE_API_URL = process.env.BASE_API_URL;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+    const timestamp = new Date()
+    console.log("post request received at " + timestamp , " looking for " + req.body.query )
+    try {
+        const { location, distance, query } = req.body;
+        const queryString = `${BASE_API_URL}?location=${encodeURIComponent(location)}&radius=${distance}&keyword=${encodeURIComponent(query)}&key=${API_KEY}&loading=async&callback=initMap`;
+        const mapsData = await axios.get(queryString);
+        res.status(200).json(mapsData.data)
+    } catch(err) {
+        console.error(err)
+        res.status(400).send(err)
+    }
+
+});
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,30 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-require("dotenv").config();
+const app = require("./app.js")
 
-const app = express();
-const API_KEY = process.env.API_KEY;
-const PORT = process.env.PORT;
-const BASE_API_URL = process.env.BASE_API_URL;
-
-app.use(express.json());
-app.use(cors());
-
-app.post("/", async (req, res) => {
-    const timestamp = new Date()
-    console.log("post request received at " + timestamp , " looking for " + req.body.query )
-    try {
-        const { location, distance, query } = req.body;
-        const queryString = `${BASE_API_URL}?location=${encodeURIComponent(location)}&radius=${distance}&keyword=${encodeURIComponent(query)}&key=${API_KEY}&loading=async&callback=initMap`;
-        const mapsData = await axios.get(queryString);
-        res.status(200).json(mapsData.data)
-    } catch(err) {
-        console.error(err)
-        res.status(400).send(err)
-    }
-
-});
+require("dotenv").config()
+const PORT = process.env.PORT||3003
 
 app.listen(PORT || 3003, () => {
   console.log(`app is live on port: ${PORT}`);
